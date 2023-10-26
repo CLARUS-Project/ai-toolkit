08b6f4666e684bdecadcb8106047fb972524a2f0
tuan vu <108931319+stormlord372@users.noreply.github.com>
2023-10-26 18:09:56 +0300

    Update read_data.py

--- Cloud/airflow/src/project_template/Data/read_data.py
+++ Cloud/airflow/src/project_template/Data/read_data.py
@@ -11,13 +11,9 @@ import json
 from io import StringIO
 import sys
 import config
-<<<<<<< Updated upstream
-# from IDS_templates.rest_ids_consumer_connector import RestIDSConsumerConnector
-=======
 import subprocess
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
->>>>>>> Stashed changes
 
 def read_data():
     try:
@@ -30,24 +26,10 @@ def read_data():
     Return:
         A Pandas DataFrame representing the content of the specified file.
     """
-<<<<<<< Updated upstream
-
-    # READ DATA FROM IDS
-    # ids_consumer = RestIDSConsumerConnector()
-    # data = ids_consumer.get_external_artifact_by_resource_title(
-    #     config.MLFLOW_EXPERIMENT, 
-    #     config.TRUE_CONNECTOR_EDGE_IP, 
-    #     config.TRUE_CONNECTOR_EDGE_PORT, 
-    #     config.TRUE_CONNECTOR_CLOUD_IP, 
-    #     config.TRUE_CONNECTOR_CLOUD_PORT
-    # )
-
-=======
     dataframe_test= pd.DataFrame([])
     #sys.path.append("dags/")
     #dataframe_test.to_csv("target1.csv")
     #print("target saved 2")
->>>>>>> Stashed changes
     # ADD YOUR CODE HERE
     #sys.path.append("C:/Users/tktuvu/Desktop/ai-toolkit/Cloud/airflow/src/project_template/")
 
